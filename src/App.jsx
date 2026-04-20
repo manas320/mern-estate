21958ddda4d096260b2a02921173cdb84e82391f
manas320 <l230772@lhr.nu.edu.pk git config --global user.email l230772@lhr.nu.edu.pk>
2026-04-20 11:45:38 +0500

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+
 
 function App() {
-  return (
-    <div className='text-4xl text-blue-500'>
-      App
-    </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+   
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+     
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App
